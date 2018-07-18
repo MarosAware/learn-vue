c9b090328ef28fff33094d0dcfcaf823f8465c39
MarosAware <marosdevone@gmail.com>
2018-07-18 20:23:00 +0200

    Component message.

--- main.js
+++ main.js
@@ -1,5 +1,70 @@
+Vue.component('message', {
+    props: ['title', 'body'],
+
+    template: `
+   <article class="message is-dark" v-show="isVisible">
+            <div class="message-header">
+                <p>{{ title }}</p>
+                <button class="delete" aria-label="delete" @click="hideModal"></button>
+            </div>
+            <div class="message-body">
+                {{ body }}
+            </div>
+        </article>
+   `,
+
+    data() {
+
+        return {
+
+            isVisible: true
+
+        };
+    },
+
+
+    // We can hide message by method, or direct in directive @click like: @click="isVisible = false";
+    methods: {
+
+        hideModal() {
+            this.isVisible = false;
+        }
+    }
+
+
+
+
+});
+
+
+
+
+
+Vue.component('task-list', {
+    template: `
+    <ul>
+        <task v-for="task in tasks" :key="task.id">{{ task.task }}</task>
+    </ul>`,
+
+    data() {
+        return {
+
+            tasks: [
+                { id:1, task: 'Go to the store', complete: true },
+                { id:2, task: 'Go to the shop', complete: false },
+                { id:3, task: 'Go to the farm', complete: true },
+                { id:4, task: 'Go to work', complete: false }
+            ]
+
+
+        };
+    }
+
+});
+
+
 Vue.component('task', {
-    template: '<li><slot></slot></li>'
+    template: '<li><slot></slot></li>',
 
     data() {
         return {
