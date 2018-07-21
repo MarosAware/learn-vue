693ead499ef24f6905522751f732ba4f2e1e55fd
MarosAware <marosdevone@gmail.com>
2018-07-21 11:33:04 +0200

    Vue cli.

---
+++ my-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+    <message>Hello World!</message>
+    <message>Hello World!</message>
+    <message>Hello World!</message>
+
+  </div>
+</template>
+
+<script>
+
+    import Message from './components/Message';
+    export default {
+        name: 'app',
+
+        components: {
+            Message
+        },
+
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ my-app/src/components/Message.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="box is-info">
+
+        <p>
+            <slot></slot>
+        </p>
+
+    </div>
+
+
+</template>
+
+
+
+
+<script>
+    export default {
+
+    }
+</script>
+
+
+
+
+<style>
+
+    .box {
+        background-color: lightgray;
+        padding: 10px;
+        border:1px solid black;
+        margin-bottom:5px;
+    }
+
+</style>
